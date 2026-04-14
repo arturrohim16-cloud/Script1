ce341b42ca35761a136b1a3e787d29c0965d4225
Ajijainalmaster <arturrohim16@gmail.com>
2026-04-15 00:48:59 +0700

    Update vps.conf download URL in ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -91,7 +91,7 @@ apt -y install nginx php php-fpm php-cli php-mysql
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 curl https://raw.githubusercontent.com/arturrohim16-cloud/Master/refs/heads/main/ssh/nginx.conf > /etc/nginx/nginx.conf
-curl https://${ajivpn}/vps.conf > /etc/nginx/conf.d/vps.conf
+curl https://raw.githubusercontent.com/arturrohim16-cloud/Master/refs/heads/main/ssh/vps.conf > /etc/nginx/conf.d/vps.conf
 sed -i 's/listen = \/var\/run\/php-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/fpm/pool.d/www.conf
 useradd -m vps;
 mkdir -p /home/vps/public_html
