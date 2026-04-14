6ceba72baa0983fe1c536f2f871c80defd8b89ef
Ajijainalmaster <arturrohim16@gmail.com>
2026-04-15 01:49:00 +0700

    Refactor setup.sh for improved installation process
    Refactor setup script to include initial checks and installation steps for various services. Removed old comments and added new functionality for downloading and executing scripts.

--- setup.sh
+++ setup.sh
@@ -32,67 +32,159 @@ apt dist-upgrade -y
 apt install -y jq curl wget sed git zip unzip build-essential net-tools socat chrony
 apt install -y libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison make libnss3-tools libevent-dev
 
-# 3. Instalasi Python & Pip (Lengkap)
-echo -e "${ORANGE}[2] Installing Python 3 and pip environment...${NC}"
-apt install -y python3 python3-pip python3-setuptools
-# Beberapa script websocket butuh library khusus
-pip3 install wheel
-pip3 install pycryptodome
+#!/bin/bash
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
 
-# 4. Sinkronisasi Waktu & Zone
-echo -e "${ORANGE}[3] Synchronizing timezone to Jakarta...${NC}"
-timedatectl set-timezone Asia/Jakarta
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$(wget -qO- ipinfo.io/ip);
 
-# 5. Membuat Struktur Folder Sistem
-mkdir -p /etc/xray
-mkdir -p /etc/v2ray
-mkdir -p /usr/bin/xray
-mkdir -p /var/lib/aji-store
-mkdir -p /var/log/xray
+rm -f setup.sh
+clear
+if [ -f "/etc/xray/domain" ]; then
+echo "Script Already Installed"
+exit 0
+fi
+mkdir /var/lib/crot;
+echo "IP=" >> /var/lib/crot/ipvps.conf
+wget https://${akbarvpn}/slhost.sh && chmod +x slhost.sh && ./slhost.sh
+#install xray
+wget https://${akbarvpnnnnnn}/ins-xray.sh && chmod +x ins-xray.sh && screen -S xray ./ins-xray.sh
+#install ssh ovpn
+wget https://${akbarvpn}/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+wget https://${akbarvpnn}/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
+#install ssr
+wget https://${akbarvpnnn}/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
+wget https://${akbarvpnnnn}/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
+#installwg
+wget https://${akbarvpnnnnn}/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
+#install L2TP
+wget https://${akbarvpnnnnnnn}/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
+wget https://${akbarvpnnnnnnnn}/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+# Websocket
+wget https://${akbarvpnnnnnnnnn}/edu.sh && chmod +x edu.sh && ./edu.sh
+# Ohp Server
+wget https://${akbarvpnnnnnnnnnn}/ohp.sh && chmod +x ohp.sh && ./ohp.sh
+# Install SlowDNS
+wget https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/SLDNS/install-sldns && chmod +x install-sldns && ./install-sldns
+# Informasi IP Saya dan Semua Port TCP UDP
+# wget https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/ipsaya.sh && chmod +x ipsaya.sh
+#
+# install xray sl-grpc
+# wget https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/grpc/sl-grpc.sh && chmod +x sl-grpc.sh && screen -S sl-grpc ./sl-grpc.sh
+# install xray grpc
+# wget https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/grpc/xray-grpc.sh && chmod +x xray-grpc.sh && screen -S xray-grpc ./xray-grpc.sh
+# install shadowsocks plugin
+# wget https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/shadowsocks-plugin/install-ss-plugin.sh && chmod +x install-ss-plugin.sh && ./install-ss-plugin.sh
+ 
 
-# 6. Menjalankan Script Instalasi Protokol (SSH, Xray, dkk)
-# Ini adalah bagian yang memanggil script yang kita buat di nomor 1-4
-echo -e "${ORANGE}[4] Executing protocol installation scripts...${NC}"
+rm -f /root/ssh-vpn.sh
+rm -f /root/sstp.sh
+rm -f /root/wg.sh
+rm -f /root/ss.sh
+rm -f /root/ssr.sh
+rm -f /root/ins-xray.sh
+rm -f /root/ipsec.sh
+rm -f /root/set-br.sh
+rm -f /root/edu.sh
+rm -f /root/ohp.sh
+rm -f /root/install
+rm -f /root/sl-grpc.sh
+rm -f /root/install-sldns
+rm -f /root/install-ss-plugin.sh
+cat <<EOF> /etc/systemd/system/autosett.service
+[Unit]
+Description=autosetting
+Documentation=nekopoi.care
 
-wget -qO ssh-vpn.sh "https://${REPO}/ssh-vpn.sh" && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
-wget -qO ins-xray.sh "https://${REPO}/ins-xray.sh" && chmod +x ins-xray.sh && ./ins-xray.sh
-wget -qO set-br.sh "https://${REPO}/set-br.sh" && chmod +x set-br.sh && ./set-br.sh
+[Service]
+Type=oneshot
+ExecStart=/bin/bash /etc/set.sh
+RemainAfterExit=yes
 
-# 7. DOWNLOAD MENU MANAGEMENT (TAHAP FINISHING)
-# Inilah bagian yang si Bos maksud, mendownload file add-ssh, add-vmess, dll.
-echo -e "${ORANGE}[5] Downloading Menu Management (Finishing)...${NC}"
-cd /usr/bin
-
-# Menu Utama
-wget -O menu "https://${REPO}/menu.sh"
-# Menu SSH & OVPN
-wget -O add-ssh "https://${REPO}/add-ssh.sh"
-wget -O del-ssh "https://${REPO}/del-ssh.sh"
-wget -O renew-ssh "https://${REPO}/renew-ssh.sh"
-wget -O cek-ssh "https://${REPO}/cek-ssh.sh"
-# Menu Xray (Vmess, Vless, Trojan)
-wget -O add-vmess "https://${REPO}/add-vmess.sh"
-wget -O del-vmess "https://${REPO}/del-vmess.sh"
-wget -O add-vless "https://${REPO}/add-vless.sh"
-wget -O del-vless "https://${REPO}/del-vless.sh"
-wget -O add-tru "https://${REPO}/add-tru.sh"
-wget -O del-tru "https://${REPO}/del-tru.sh"
-# Menu Tambahan
-wget -O cert-xray "https://${REPO}/cert-xray.sh"
-wget -O speedtest "https://${REPO}/speedtest_worker.py"
-wget -O info "https://${REPO}/info.sh"
-wget -O about "https://${REPO}/about.sh"
-
-# Memberikan Izin Eksekusi ke Semua Menu
-chmod +x menu add-ssh del-ssh renew-ssh cek-ssh add-vmess del-vmess add-vless del-vless add-tru del-tru cert-xray speedtest info about
-
-# 8. Finalisasi
-echo -e "${GREEN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-echo -e "${GREEN}    SYSTEM SETUP & FINISHING COMPLETED!         ${NC}"
-echo -e "${GREEN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-echo -e "${PURPLE}  Ketik 'menu' untuk melihat daftar perintah.    ${NC}"
-echo -e "${GREEN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl enable autosett
+wget -O /etc/set.sh "https://${akbarvpn}/set.sh"
+chmod +x /etc/set.sh
+history -c
+echo "1.2" > /home/ver
+echo " "
+echo "Installation has been completed!!"echo " "
+echo "============================================================================" | tee -a log-install.txt
+echo "" | tee -a log-install.txt
+echo "----------------------------------------------------------------------------" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Service & Port"  | tee -a log-install.txt
+echo "   - SlowDNS SSH             : ALL Port SSH"  | tee -a log-install.txt
+echo "   - OpenSSH                 : 22, 2253"  | tee -a log-install.txt
+echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 990"  | tee -a log-install.txt
+echo "   - Stunnel5                : 443, 445"  | tee -a log-install.txt
+echo "   - Dropbear                : 443, 109, 143"  | tee -a log-install.txt
+echo "   - CloudFront Websocket    : "  | tee -a log-install.txt
+echo "   - SSH Websocket TLS       : 443"  | tee -a log-install.txt
+echo "   - SSH Websocket HTTP      : 8880"  | tee -a log-install.txt
+echo "   - Websocket OpenVPN       : 2086"  | tee -a log-install.txt
+echo "   - Squid Proxy             : 3128, 8080"  | tee -a log-install.txt
+echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
+echo "   - Nginx                   : 89"  | tee -a log-install.txt
+echo "   - Wireguard               : 7070"  | tee -a log-install.txt
+echo "   - L2TP/IPSEC VPN          : 1701"  | tee -a log-install.txt
+echo "   - PPTP VPN                : 1732"  | tee -a log-install.txt
+echo "   - SSTP VPN                : 444"  | tee -a log-install.txt
+echo "   - Shadowsocks-R           : 1443-1543"  | tee -a log-install.txt
+echo "   - SS-OBFS TLS             : 2443-2543"  | tee -a log-install.txt
+echo "   - SS-OBFS HTTP            : 3443-3543"  | tee -a log-install.txt
+echo "   - XRAYS Vmess TLS         : 8443"  | tee -a log-install.txt
+echo "   - XRAYS Vmess None TLS    : 80"  | tee -a log-install.txt
+echo "   - XRAYS Vless TLS         : 8443"  | tee -a log-install.txt
+echo "   - XRAYS Vless None TLS    : 80"  | tee -a log-install.txt
+echo "   - XRAYS Trojan            : 2083"  | tee -a log-install.txt
+echo "   - XRAYS VMESS GRPC        : 1180"  | tee -a log-install.txt
+echo "   - XRAYS VLESS GRPC        : 2280"  | tee -a log-install.txt
+echo "   - OHP SSH                 : 8181"  | tee -a log-install.txt
+echo "   - OHP Dropbear            : 8282"  | tee -a log-install.txt
+echo "   - OHP OpenVPN             : 8383"  | tee -a log-install.txt
+echo "   - TrojanGo                : 2087"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
+echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
+echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
+echo "   - IPtables                : [ON]"  | tee -a log-install.txt
+echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
+echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
+echo "   - Autoreboot On 05.00 GMT +7" | tee -a log-install.txt
+echo "   - Autobackup Data" | tee -a log-install.txt
+echo "   - Restore Data" | tee -a log-install.txt
+echo "   - Auto Delete Expired Account" | tee -a log-install.txt
+echo "   - Full Orders For Various Services" | tee -a log-install.txt
+echo "   - White Label" | tee -a log-install.txt
+echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
+echo " Reboot 15 Sec"
+sleep 15
+rm -f setup.sh
+reboot
 # Membersihkan file installer sementara
 cd /root
 rm -f ssh-vpn.sh ins-xray.sh set-br.sh setup.sh
