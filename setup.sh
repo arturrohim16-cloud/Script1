95a401fae21f0329223b304db8b9c1101d8e77d3
Ajijainalmaster <arturrohim16@gmail.com>
2026-04-15 02:50:36 +0700

    Update wget commands to use raw GitHub URLs

--- setup.sh
+++ setup.sh
@@ -80,7 +80,7 @@ wget https://raw.githubusercontent.com/arturrohim16-cloud/Master/refs/heads/main
 #wget https://${akbarvpnnnnnnn}/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
 wget https://raw.githubusercontent.com/arturrohim16-cloud/Script1/refs/heads/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 # Websocket
-wget https://${akbarvpnnnnnnnnn}/edu.sh && chmod +x edu.sh && ./edu.sh
+wget https://raw.githubusercontent.com/arturrohim16-cloud/Script1/refs/heads/main/edu.sh && chmod +x edu.sh && ./edu.sh
 # Ohp Server
 wget https://${akbarvpnnnnnnnnnn}/ohp.sh && chmod +x ohp.sh && ./ohp.sh
 # Install SlowDNS
