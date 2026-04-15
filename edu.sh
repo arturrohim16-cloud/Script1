3270f38e4ae951cc92182a1227e7f7328b38a1a3
Ajijainalmaster <arturrohim16@gmail.com>
2026-04-15 13:05:19 +0700

    Add ws-stunnel installation and service setup steps

--- edu.sh
+++ edu.sh
@@ -31,3 +31,38 @@ systemctl restart ws-python
 
 # 5. Cek status apakah sudah berjalan (Running)
 systemctl status ws-python
+
+
+# 1. Download script dari Github Anda
+wget -O /usr/local/bin/ws-stunnel "https://raw.githubusercontent.com/arturrohim16-cloud/Script1/refs/heads/main/ws-stunnel"
+
+# 2. Beri izin eksekusi
+chmod +x /usr/local/bin/ws-stunnel
+
+# 3. Buat file service Systemd
+cat > /etc/systemd/system/ws-stunnel.service <<EOF
+[Unit]
+Description=Python WebSocket TLS Proxy Aji Store
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python3 /usr/local/bin/ws-stunnel
+Restart=on-failure
+RestartSec=3s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# 4. Aktifkan dan Jalankan Service
+systemctl daemon-reload
+systemctl enable ws-stunnel
+systemctl restart ws-stunnel
+
+# 5. Cek Status
+systemctl status ws-stunnel
